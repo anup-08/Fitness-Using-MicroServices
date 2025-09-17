0b7a597b06343d180b3e78ba5800d1c4a498c87e
Anup Kumar <209731026+anup-08@users.noreply.github.com>
2025-09-17 14:35:42 +0530

    Update Recommendation.java

--- Backend-SpringBoot/Ai-Service/src/main/java/com/aiService/model/Recommendation.java
+++ Backend-SpringBoot/Ai-Service/src/main/java/com/aiService/model/Recommendation.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collation = "aiServices")
+@Document(collection  = "aiServices")
 @Data
 @Builder
 public class Recommendation {
